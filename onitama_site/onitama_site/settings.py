0febe2f40eff13ebffb2c9df86ab6c5c69d4887d
Adrian Duynstee <adrian.duy@gmail.com>
2018-07-03 16:35:30 -0700

    Add installed app to settings

--- onitama_site/onitama_site/settings.py
+++ onitama_site/onitama_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'onitama_game.apps.OnitamaGameConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
