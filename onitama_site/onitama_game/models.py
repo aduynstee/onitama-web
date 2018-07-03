10684f4961451bf41b0180a97f8c1b8d78141ecc
Adrian Duynstee <adrian.duy@gmail.com>
2018-07-03 16:31:35 -0700

    Choices for player field

--- onitama_site/onitama_game/models.py
+++ onitama_site/onitama_game/models.py
@@ -7,8 +7,12 @@ class Game(models.Model):
 
 
 class Move(models.Model):
+    PLAYER_CHOICES = (
+        ('R', 'Red'),
+        ('B', 'Blue'),
+    )
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    player = models.CharField(max_length=10)
+    player = models.CharField(max_length=1, choices=PLAYER_CHOICES)
     turn = models.PositiveSmallIntegerField()
     start = models.CharField(max_length=2)
     end = models.CharField(max_length=2)
