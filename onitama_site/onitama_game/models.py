892517c747430174dd3f83bf7e196a169664b770
Adrian Duynstee <adrian.duy@gmail.com>
2018-07-07 13:51:01 -0700

    Card model.  Explicit choices for Move.start and Move.end

--- onitama_site/onitama_game/models.py
+++ onitama_site/onitama_game/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Game(models.Model):
     start_date = models.DateTimeField()
-    cards = models.CharField(max_length=200)
+    cards = models.ManyToManyField("Card", through="GameCard")
 
 
 class Move(models.Model):
@@ -11,9 +11,27 @@ class Move(models.Model):
         ('R', 'Red'),
         ('B', 'Blue'),
     )
+    LOCATION_CHOICES = tuple(
+        (x+y, x+y) for x in ['A', 'B', 'C', 'D', 'E'] for y in ['1', '2', '3', '4', '5']
+    )
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     player = models.CharField(max_length=1, choices=PLAYER_CHOICES)
     turn = models.PositiveSmallIntegerField()
-    start = models.CharField(max_length=2)
-    end = models.CharField(max_length=2)
-    card = models.CharField(max_length=10)
+    start = models.CharField(max_length=2, choices=LOCATION_CHOICES)
+    end = models.CharField(max_length=2, choices=LOCATION_CHOICES)
+    card = models.ForeignKey("Card", on_delete=models.CASCADE)
+
+
+class Card(models.Model):
+    name = models.CharField(max_length=10)
+
+
+class GameCard(models.Model):
+    CARDHOLDER_CHOICES = (
+        ('R', 'Red'),
+        ('B', 'Blue'),
+        ('N', 'Neutral'),
+    )
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    cardholder = models.CharField(max_length=1, choices=CARDHOLDER_CHOICES)
