972371564ea8e018713bfc5b1955d2045d58680b
Adrian Duynstee <adrian.duy@gmail.com>
2018-07-08 23:03:25 -0700

    Methods for converting models to 'live' objects that can be manipulated by the onitama.py module (game logic module)

--- onitama_site/onitama_game/models.py
+++ onitama_site/onitama_game/models.py
@@ -1,9 +1,24 @@
+from .modules import onitama as oni
+from .exceptions import GameIntegrityError
 from django.db import models
 
 
 class Game(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     cards = models.ManyToManyField("Card", through="GameCard")
+
+    def as_live_game(self):
+        gc = GameCard.objects.filter(game=self)
+        red = [x.card.as_live_card() for x in gc.filter(cardholder='R')]
+        blue = [x.card.as_live_card() for x in gc.filter(cardholder='B')]
+        neutral = [gc.filter(cardholder='N').first().card.as_live_card()]
+        live_game = oni.Game(red+blue+neutral)
+        for move in self.move_set.all():
+            try:
+                live_game.do_move(move.as_live_move())
+            except oni.IllegalMoveError:
+                raise GameIntegrityError(self, move)
+        return live_game
 
 
 class Move(models.Model):
@@ -25,9 +40,21 @@ class Move(models.Model):
         ordering = ["turn"]
         unique_together = ("game", "turn")
 
+    def as_live_move(self):
+        player = oni.Player.RED if self.player == 'R' else oni.Player.BLUE
+        x = ['A', 'B', 'C', 'D', 'E']
+        y = ['1', '2', '3', '4', '5']
+        start = (x.index(self.start[0]), y.index(self.start[1]))
+        end = (x.index(self.end[0]), y.index(self.end[1]))
+        card = self.card.as_live_card()
+        return oni.Move(player, start, end, card)
+
 
 class Card(models.Model):
     name = models.CharField(max_length=10)
+
+    def as_live_card(self):
+        return oni.NAME_TO_CARD[self.name.lower()]
 
 
 class GameCard(models.Model):
