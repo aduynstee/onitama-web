75a8b69f8618983d65b6721db9122ad848b40e37
Adrian Duynstee <adrian.duy@gmail.com>
2018-07-22 13:38:48 -0700

    Build react app and serve with django

--- onitama_site/onitama_game/urls.py
+++ onitama_site/onitama_game/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('game/<int:game_id>/', views.game, name='game'),
+    path('react/', TemplateView.as_view(template_name='index.html'))
 ]
--- onitama_site/onitama_site/settings.py
+++ onitama_site/onitama_site/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'onitama_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'onitama_game/reactbuild')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'onitama_game/reactbuild/static'),
+]
 
 # Channels
 ASGI_APPLICATION = 'onitama_site.routing.application'
