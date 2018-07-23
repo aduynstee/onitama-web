6926e83c2100031d45ccfd1bcbc6484eccba400b
Adrian Duynstee <adrian.duy@gmail.com>
2018-07-23 16:39:11 -0700

    Convenient script for copying over new react app builds and updating game.html template with new script

---
+++ frontend/oni-app/copybuild.bat
@@ -0,0 +1,3 @@
+del /Q ..\..\onitama_site\onitama_game\reactbuild\*
+xcopy .\build\* ..\..\onitama_site\onitama_game\reactbuild\ /E
+python update_game_template.py
---
+++ frontend/oni-app/update_game_template.py
@@ -0,0 +1,16 @@
+import os
+import re
+
+pattern = re.compile(r"main\.(?P<name>.+)\.js")
+directory = r'..\..\onitama_site\onitama_game\reactbuild\static\js'
+
+for filename in os.listdir(directory):
+    if filename.endswith(".js"):
+        result = pattern.search(filename)
+        output_name = result.group("name")
+        print(output_name)
+
+template = r'<!DOCTYPE html><html><head><meta charset="utf-8"/><link href="/static/css/main.a8da6748.css" rel="stylesheet"><title>Game</title></head><body>{{game}}<div id="root"></div><script>window.socketAddress = "{{socket_url}}";</script><script type="text/javascript" src="/static/js/main.'+output_name+'.js"></script></body></html>'
+
+with open(r'..\..\onitama_site\onitama_game\templates\onitama_game\game.html', 'w') as f:
+    f.write(template)
