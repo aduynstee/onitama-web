b8e11be60d0c4f8fc21842dcc92f5d0cf0e25d52
Adrian Duynstee <adrian.duy@gmail.com>
2018-07-25 23:33:14 -0700

    Render cards

--- frontend/oni-app/src/App.js
+++ frontend/oni-app/src/App.js
@@ -197,12 +197,14 @@ class Game extends Component {
                                 onClick={
                                     () => this.selectCard(this.state.cards[2])
                                 }
+                                flipCard={true}
                             />
                             <Card
                                 name={this.state.cards[3]}
                                 onClick={
                                     () => this.selectCard(this.state.cards[3])
                                 }
+                                flipCard={true}
                             />
                         </div>
                         <Board
@@ -243,16 +245,83 @@ class Game extends Component {
     }
 }
 
+const cardData = {
+    "monkey": [16, 8, 18, 6],
+    "elephant": [11, 13, 16, 18],
+    "crane": [8, 17, 6],
+    "mantis": [16, 18, 7],
+    "tiger": [22, 7],
+    "dragon": [15, 8, 6, 19],
+    "boar": [11, 13, 17],
+    "crab": [14, 17, 10],
+    "goose": [11, 13, 16, 8],
+    "rooster": [11, 13, 6, 18],
+    "eel": [13, 16, 6],
+    "cobra": [11, 8, 18],
+    "horse": [11, 17, 7],
+    "ox": [13, 17, 7],
+    "frog": [16, 8, 10],
+    "rabbit": [14, 6, 18],
+};
+
 class Card extends Component {
     render() {
-        return (
-            <div
-                className="card"
-                onClick = {this.props.onClick}
-            >
-                {this.props.name}
-            </div>
-        )
+        let filled = cardData[this.props.name];
+        let rows = [];
+        for (let i = 0; i < 5; i++) {
+            let row = [];
+            for (let j = 0; j < 5; j++) {
+                let cls = "";
+                if (filled.includes(i*5 + j)) {
+                    cls = "cardgrid-square filled";
+                } else if (i*5 + j === 12) {
+                    cls = "cardgrid-square center";
+                }
+                else {
+                    cls = "cardgrid-square blank";
+                }
+                row.push(
+                    <div
+                        key={i+j}
+                        className={cls}
+                    ></div>
+                );
+            }
+            if (this.props.flipCard) {
+                row.reverse();
+            }
+            rows.push(
+                <div
+                    key={i}
+                    className="cardgrid-row"
+                >
+                    {row}
+                </div>
+            );
+        }
+        if (this.props.flipCard) {
+            return (
+                <div
+                    className="card"
+                    onClick = {this.props.onClick}
+                >
+                    {this.props.name}
+                    {rows}
+                </div>
+            )
+        } else {
+            rows.reverse();
+            return (
+                <div
+                    className="card"
+                    onClick = {this.props.onClick}
+                >
+                    {rows}
+                    {this.props.name}
+                </div>
+            )
+        }
+
     }
 }
 
