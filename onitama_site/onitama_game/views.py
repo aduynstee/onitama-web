19ff77dc707a373c01fe97d291f76f7b36a69ab1
Adrian Duynstee <adrian.duy@gmail.com>
2018-07-29 20:42:38 -0700

    Immediately add creator to new game

--- onitama_site/onitama_game/views.py
+++ onitama_site/onitama_game/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.sessions.backends.base import UpdateError
-from .models import Game
+from django.contrib.sessions.models import Session
+from .models import Game, Player
 
 def index(request):
     game_list = Game.objects.all()
@@ -26,5 +27,14 @@ def game(request, game_id):
     )
 
 def new(request):
+    try:
+        # Need to manually save to ensure Session is stored (will be needed later)
+        request.session.save()
+    except UpdateError:
+        # If Session was deleted from database must issue a new one
+        request.session.flush()
+        request.session.save()
+    session = Session.objects.get(pk=request.session.session_key)
     game = Game.create()
+    game.add_player(session)
     return redirect('game', game_id=game.id)
