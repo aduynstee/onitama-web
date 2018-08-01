0cfbbe8b992409864b90b1ea821f979f4cb92d3d
Adrian Duynstee <adrian.duy@gmail.com>
2018-07-31 19:53:56 -0700

    Fix Python 3.7 specific bug: escape character not allowed in replacement string when using re.sub

--- frontend/oni-app/update_game_template.py
+++ frontend/oni-app/update_game_template.py
@@ -15,8 +15,8 @@ for filename in os.listdir(css_directory):
     if filename.endswith(".css"):
         stylesheet_name = pattern.search(filename).group(0)
 
-script = '"\static\js\{}"'.format(script_name)
-stylesheet = '"\static\css\{}"'.format(stylesheet_name)
+script = r'"\static\js\{}"'.format(script_name)
+stylesheet = r'"\static\css\{}"'.format(stylesheet_name)
 
 style_pat = re.compile(r';;game_stylesheet;;')
 script_pat = re.compile(r';;game_script;;')
@@ -24,6 +24,8 @@ script_pat = re.compile(r';;game_script;;')
 with open(r'..\..\onitama_site\onitama_game\templates\onitama_game\game.template.html', 'r') as template:
     with open(r'..\..\onitama_site\onitama_game\templates\onitama_game\game.html', 'w') as output:
         for line in template:
-            line = re.sub(script_pat, script, line)
-            line = re.sub(style_pat, stylesheet, line)
+            # Use a function instead of string for second argument
+            # because escape characters are not allowed in Python 3.7+
+            line = re.sub(script_pat, lambda x: script, line)
+            line = re.sub(style_pat, lambda x: stylesheet, line)
             output.write(line)
